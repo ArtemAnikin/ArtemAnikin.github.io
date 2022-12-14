cfd2e25eb89fca8bfd1f614dbaaa8d3586334afd
ArtemAnikin <Artem.anikin.2015@gmail.com>
2022-12-14 16:18:32 +0100

    add arrow listeners

--- src/components/ui/ReachDate/ReachDate.tsx
+++ src/components/ui/ReachDate/ReachDate.tsx
@@ -21,12 +21,28 @@ const ReachDate: FC<IReachDateProps> = ({ changeNumberOfMonth, label }) => {
 	const [year, setYear] = useState(currentYear)
 
 	const wrapperRef = useRef<HTMLDivElement>(null)
+	const leftArrowBtn = useRef<HTMLButtonElement>(null)
+	const rightArrowBtn = useRef<HTMLButtonElement>(null)
 
 	useEffect(() => {
-		if (wrapperRef) {
-			wrapperRef.current?.focus()
+		wrapperRef.current?.focus()
+		const handlerArrows = (event: any) => {
+			switch (event.key) {
+				case 'ArrowRight':
+					rightArrowBtn.current?.click()
+					break
+				case 'ArrowLeft':
+					leftArrowBtn.current?.click()
+					break
+			}
 		}
-	})
+
+		wrapperRef.current?.addEventListener('keydown', handlerArrows)
+
+		return () => {
+			wrapperRef.current?.removeEventListener('keydown', handlerArrows)
+		}
+	}, [month, wrapperRef])
 
 	const handleMonthInc = () => {
 		changeMonth(12, 1)
@@ -57,14 +73,22 @@ const ReachDate: FC<IReachDateProps> = ({ changeNumberOfMonth, label }) => {
 				data-testid='Reach-date'
 				tabIndex={0}
 			>
-				<button className={styles.leftArrowButton} onClick={handleMonthDec}>
+				<button
+					className={styles.leftArrowButton}
+					ref={leftArrowBtn}
+					onClick={() => handleMonthDec()}
+				>
 					<img src={leftArrow} alt='left arrow' />
 				</button>
 				<div className={styles.date}>
 					<p>{monthFormatter(month)}</p>
 					<p className={styles.year}>{year}</p>
 				</div>
-				<button className={styles.rightArrowButton} onClick={handleMonthInc}>
+				<button
+					className={styles.rightArrowButton}
+					ref={rightArrowBtn}
+					onClick={() => handleMonthInc()}
+				>
 					<img src={rightArrow} alt='right arrow' />
 				</button>
 			</div>
